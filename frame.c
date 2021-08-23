7e181cb4df49af1870b7e7a3ac3e1edeb333d69d
hanqing <602997434@qq.com>
2021-08-23 04:03:29 -0700

    frame mod

--- frame.c
+++ frame.c
@@ -2,7 +2,28 @@
 // skynet框架搭建
 /*
     linux环境搭建：
+        安装VMware：
+        配置静态IP：sudo mv /etc/netplan/01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml.bak、sudo vim /etc/netplan/01-network-manager-all.yaml
+                   # create new
+                   network:
+                     ethernets:
+                       ens33:
+                         dhcp4: no
+                         addresses: [192.168.0.53/24]
+                         gateway4: 192.168.0.254
+                         nameservers:
+                           addresses: [114.114.114.114]
+                         dhcp6: no
+                     version: 2
 
+        安装vim：sudo apt-get install vim
+        安装git：sudo apt-get install git
+        安装readline-devel：sudo apt-get install libreadline-dev
+        安装autoconf：sudo apt-get install autoconf
+        安装openssh-server：sudo apt-get update、sudo apt-get install openssh-server
+        安装samba：sudo apt install samba、修改smb.conf、chown -R samba:samba /home/server、sudo systemctl start smbd、sudo systemctl enable smbd
     protobuf协议接入：
-    
+        编译安装luarocks：
+        编译安装lua-protobuf：
+        protobuf转pb文件：
 */
