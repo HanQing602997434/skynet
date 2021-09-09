f1d49551dddaaaf78dda95c7fcd149a2277306af
hq <602997434@qq.com>
2021-09-08 20:30:17 -0700

    sharedata mod

--- sharedata.c
+++ sharedata.c
@@ -75,5 +75,57 @@
         sharedata是基于共享内存工作的，且访问共享对象内的数据并不会阻塞当前服务。所以可以保证不错的性能，并节省大量
         的内存。
 
-        sharedata
+        sharedata的缺点是更新一次的成本非常大，所以不适合做服务间的数据交换。你可以考虑它的替代品：stm模块。
+
+        sharedata的实现是将原来的数据存储在自定义的table中，并在访问上保持原来的层级结构。自定义的table是一个
+        userdata，其它lua VM通过自定义table的指针访问自定义table中的内容。corelib.lua中自定义了userdata的
+        元表。元表中包含__index、__len、__pairs等方法，使得访问userdata像访问原有数据一样。
+
+        如果你仅仅想利用sharedata模块分发配置表数据，而不关心自动更新数据、共享一部分内存。那么可以使用更高效的接口：
+
+        sharedata.deepcopy(name, key, ...)这个api会获得name对应的数据表，并根据你需要的key将数据做一次深拷贝，生
+        成一次lua table。这样的访问效率更高。例如：sharedata.deepcopy("foobar", "x", "y")会返回foobar.x.y的内容。
+
+    stm：
+
+        stm（software transactional memory）模块同样基于共享内存，所以也只能用于同一个skynet节点内。它是一个试验性
+        模块，不一定比消息传递的方式更好。只是提供一个新思路来进行同一节点内的服务间数据交换。
+
+        因为它不经过skynet的消息投递，信息投递的时效性比消息投递要好一些。但由于依旧需要在不同的lua VM间交换数据，序
+        列化（使用skynet.pack和skynet.unpack）必不可少。因为绕过了消息投递，它还可以用于广播。多个读取者可以同时去读
+        一个写入者更新的数据。
+
+        stm是以一个C编写的lua模块形式提供的。
+
+            local stm = require "skynet.stm"
+
+        可以引入这个模块。由于api多是操作C指针，所以调用其中的api上需要小心（否则会有内存泄露）。
+
+            1.stm.new(pointer, size)可以生成一个共享对象，生成者可以改写这个对象，pointer/size是一个C指针以及长度。
+              skynet.pack可以正确生成它们。它返回一个stmobj，是一个userdata，lua的gc会正确的回收它引用的内存。
+            2.stm.copy(stmobj)可以从一个共享对象中生成一份读拷贝。它返回一个stmcopy，是一个lightuserdata。通常一定
+              要把这个lightuserdata传到需要读取这份数据的服务。随意丢弃这个指针会导致内存泄露。注：一个拷贝只能提供
+              一个读取者使用，你不可以把这个指针传递给多服务。如果你有多个读取者，为每个人调用copy生成一份读拷贝。
+            3.stm.newcopy(stmcopy)把一个C指针转换为一份读拷贝。只有经过转换，stm才能正确的管理它的声明周期。
+
+        持有stmobj，则是这个共享对象的写入者。你可以用stmobj(pointer, size)的方式更新其中携带的信息。（这个userdata
+        重载了call方法）。
+
+        持有stmcopy，则是这个共享对象的读取者。stmcpy是用stm.copy生成的那个指针，传递给stm.newcopy构造出来的。你可以
+        用stmcopy(function(pointer, size, [, ud])...end [, ud])的方式读出其中的数据。如果数据没有更新，将返回flase；
+        否则，将更新过的数据指针pointer以及长度，传递给传入的反序列化函数，并返回true以及反序列化函数的结果。
+
+        test/teststm.lua是一个简单的范例。
+
+    sharemap：
+
+        sharemap是对stm的简单应用。你可以用sharemap创建一个对象负责读写一张预定义的数据结构（使用sproto描述结果）。
+        然后构造出读对象传递给其它服务。
+
+        当读写方修改了数据内容后，可以通过调用commit将修改后的副本同步给所有读取方。而读取方则需要主动调用update获得
+        最新副本。
+
+        test/testsm.lua是一个简单的使用范例。
+        
+        注意：如果需要同步的数据结构较大，这种方式的成本也会增加。因为每次commit都会全量序列化整个结构。
 */
